02b95bf9f4967c181e86a67a674006cdea7de3b7
hoyinli1211 <hoyinli.r@gmail.com>
2023-08-30 23:42:36 +0800

    Update App.py

--- App.py
+++ App.py
@@ -6,7 +6,7 @@ from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 # Specify model name
 model_name = "bert-large-uncased-whole-word-masking-finetuned-squad"
 
-# Load the model and tokenizer
+# Function to load model and tokenizer
 @st.cache(allow_output_mutation=True)
 def load_model_and_tokenizer():
     model = AutoModelForQuestionAnswering.from_pretrained(model_name)
@@ -25,7 +25,7 @@ question = st.text_input('Question')
 
 # Button to get answer
 if st.button('Get Answer'):
-    inputs = tokenizer(question, context, return_tensors='pt')
+    inputs = tokenizer.encode_plus(question, context, truncation=True, padding='max_length', max_length=512, return_tensors='pt')
     outputs = model(**inputs)
     answer_start = torch.argmax(outputs.start_logits)  # get the most likely beginning of answer with the argmax of the start logits
     answer_end = torch.argmax(outputs.end_logits) + 1  # get the most likely end of answer with the argmax of the end logits
